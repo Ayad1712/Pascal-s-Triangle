7f0c7965467d0fa49d939eeaa8327a154edc0bbc
Ayad1712 <ayadhaider06@gmail.com>
2025-04-06 11:56:28 -0400

    pascal.py
    update (part 1)

--- pascal.py
+++ pascal.py
@@ -1,1 +1,10 @@
 #new pascal code here
+
+#3 
+number = int(input("Type a number"))
+final_list = []
+num = [1]
+for i in range(1,number+1):
+    sublist = num * i
+    final_list.append(sublist)
+    print(final_list)
